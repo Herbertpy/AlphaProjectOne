334f6b97497e4c958c366bbeb05ee7f2f7a7686b
Herbert <103264430+Herbertpy@users.noreply.github.com>
2023-01-28 00:31:48 +0530

    Herbert pusing the code

--- src/test/java/org/alpha/AlphaOne.java
+++ src/test/java/org/alpha/AlphaOne.java
@@ -1,5 +1,15 @@
 package org.alpha;
 
 public class AlphaOne {
+	
+	private void herbertOne() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void herbertTwo() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
