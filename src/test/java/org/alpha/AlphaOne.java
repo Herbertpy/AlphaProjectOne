9194fe956573c4e6148ea272b77d9fe8393e58bc
Herbert <103264430+Herbertpy@users.noreply.github.com>
2023-01-27 21:13:31 +0530

    pushing code into vimal

--- src/test/java/org/alpha/AlphaOne.java
+++ src/test/java/org/alpha/AlphaOne.java
@@ -1,5 +1,14 @@
 package org.alpha;
 
 public class AlphaOne {
+	
+	private void vimalOne() {
+		// TODO Auto-generated method stub
+
+	}
+	private void vimalTwo() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
