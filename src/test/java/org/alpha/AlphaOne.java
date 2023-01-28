a7c74a6d96215a28f4c171e3a89c12c2e0f44ef0
Pachamuthuv <pachamuthuvengg@gmail.com>
2023-01-28 15:37:11 +0530

    add  muthu method

--- src/test/java/org/alpha/AlphaOne.java
+++ src/test/java/org/alpha/AlphaOne.java
@@ -1,5 +1,13 @@
 package org.alpha;
 
 public class AlphaOne {
+	private void muthuOne() {
+		// TODO Auto-generated method stub
+
+	}
+	private void muthuTwo() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
